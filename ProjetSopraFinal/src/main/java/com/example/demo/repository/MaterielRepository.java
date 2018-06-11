bf5203689f7d08c982d5f7a610e0a34659c57088
Unknown <alexisrelin@gmail.com>
2018-06-11 11:10:40 +0200

    passage int -> Long ( attribut code)

--- ProjetSopraFinal/src/main/java/com/example/demo/repository/MaterielRepository.java
+++ ProjetSopraFinal/src/main/java/com/example/demo/repository/MaterielRepository.java
@@ -7,7 +7,7 @@ import com.example.demo.model.Materiel;
 
 public interface MaterielRepository extends JpaRepository<Materiel, Long> {
 
-	public Materiel findByCode(@Param("code") Integer code);
+	public Materiel findByCode(@Param("code") Long code);
 
 	public Materiel findByCout(@Param("cout") Integer cout);
 }

0939f2cf001f08b2b93b79690bfc8da131eb3e24
Unknown <sisto94000@hotmail.fr>
2018-06-11 17:13:55 +0200

    modif pom et security

--- ProjetSopraFinal/src/main/java/com/example/demo/configuration/SecurityConfiguration.java
+++ ProjetSopraFinal/src/main/java/com/example/demo/configuration/SecurityConfiguration.java
@@ -1,62 +1,62 @@
-package com.example.demo.configuration;
-
-import javax.sql.DataSource;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-
-import com.example.demo.service.CustomUserDetailsService;
-
-@Configuration
-public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
-
-	@Autowired
-	DataSource dataSource;
-	@Autowired
-	CustomUserDetailsService customUserDetailsService;
-
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		//http.authorizeRequests().antMatchers("/hello", "/sec/welcome").permitAll().antMatchers("/sec/secure")
-		//		.authenticated().and().formLogin();
-		http.authorizeRequests().antMatchers("/rest/personne/**").authenticated().and().httpBasic();
-		http.csrf().disable();
-		http.headers().frameOptions().disable();
-	}
-	// authentification avec info directement en memoire
-	// @Autowired
-	// public void configAuthentication(AuthenticationManagerBuilder auth)
-	// throws Exception{
-	// auth.inMemoryAuthentication().withUser("simon").password("{noop}toto").roles("ADMIN");
-	// }
-
-	// authentifcation avec requete sql
-	// @Autowired
-	// public void configAuthentication(AuthenticationManagerBuilder auth)
-	// throws Exception {
-	// auth.jdbcAuthentication().dataSource(dataSource)
-	// .usersByUsernameQuery("select username,password,enable from users where
-	// username=?")
-	// .authoritiesByUsernameQuery("select username, role from user_roles where
-	// username=?");
-	// }
-
-	@Autowired
-	public void configAuthentication(AuthenticationManagerBuilder auth) throws Exception {
-		System.out.println(getPasswordEncoder().encode("toto"));
-		auth.userDetailsService(customUserDetailsService).passwordEncoder(getPasswordEncoder());
-
-	}
-	
-	@Bean(name="passwordEncoder")
-	public PasswordEncoder getPasswordEncoder(){
-		return new BCryptPasswordEncoder();
-	}
-}
+//package com.example.demo.configuration;
+//
+//import javax.sql.DataSource;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+//import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//import org.springframework.security.crypto.password.PasswordEncoder;
+//
+//import com.example.demo.service.CustomUserDetailsService;
+//
+//@Configuration
+//public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+//
+//	@Autowired
+//	DataSource dataSource;
+//	@Autowired
+//	CustomUserDetailsService customUserDetailsService;
+//
+//	@Override
+//	protected void configure(HttpSecurity http) throws Exception {
+//		//http.authorizeRequests().antMatchers("/hello", "/sec/welcome").permitAll().antMatchers("/sec/secure")
+//		//		.authenticated().and().formLogin();
+//		http.authorizeRequests().antMatchers("/rest/personne/**").authenticated().and().httpBasic();
+//		http.csrf().disable();
+//		http.headers().frameOptions().disable();
+//	}
+//	// authentification avec info directement en memoire
+//	// @Autowired
+//	// public void configAuthentication(AuthenticationManagerBuilder auth)
+//	// throws Exception{
+//	// auth.inMemoryAuthentication().withUser("simon").password("{noop}toto").roles("ADMIN");
+//	// }
+//
+//	// authentifcation avec requete sql
+//	// @Autowired
+//	// public void configAuthentication(AuthenticationManagerBuilder auth)
+//	// throws Exception {
+//	// auth.jdbcAuthentication().dataSource(dataSource)
+//	// .usersByUsernameQuery("select username,password,enable from users where
+//	// username=?")
+//	// .authoritiesByUsernameQuery("select username, role from user_roles where
+//	// username=?");
+//	// }
+//
+//	@Autowired
+//	public void configAuthentication(AuthenticationManagerBuilder auth) throws Exception {
+//		System.out.println(getPasswordEncoder().encode("toto"));
+//		auth.userDetailsService(customUserDetailsService).passwordEncoder(getPasswordEncoder());
+//
+//	}
+//	
+//	@Bean(name="passwordEncoder")
+//	public PasswordEncoder getPasswordEncoder(){
+//		return new BCryptPasswordEncoder();
+//	}
+//}
--- ProjetSopraFinal/src/main/java/com/example/demo/service/CustomUserDetailsService.java
+++ ProjetSopraFinal/src/main/java/com/example/demo/service/CustomUserDetailsService.java
@@ -1,33 +1,33 @@
-package com.example.demo.service;
-
-import com.example.demo.model.*;
-import com.example.demo.repository.RessourceHumaineRepository;
-import com.example.demo.repository.RessourceHumaineRoleRepository;
-
-import java.util.Optional;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.stereotype.Service;
-
-@Service
-public class CustomUserDetailsService implements UserDetailsService {
-
-	@Autowired
-	RessourceHumaineRepository RHRepository;
-	@Autowired
-	RessourceHumaineRoleRepository RHRoleRepository;
-
-	@Override
-	public UserDetails loadUserByUsername(String arg0) throws UsernameNotFoundException {
-		Optional<RessourceHumaine> opt= RHRepository.findByLogin(arg0);
-		if (!opt.isPresent()) {
-			throw new UsernameNotFoundException("utilisateur inconnu");
-		}
-		return new CustomUserDetails(opt.get(), RHRoleRepository.findCustomRoleByLogin(arg0));
-
-	}
-
-}
+//package com.example.demo.service;
+//
+//import com.example.demo.model.*;
+//import com.example.demo.repository.RessourceHumaineRepository;
+//import com.example.demo.repository.RessourceHumaineRoleRepository;
+//
+//import java.util.Optional;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.security.core.userdetails.UserDetails;
+//import org.springframework.security.core.userdetails.UserDetailsService;
+//import org.springframework.security.core.userdetails.UsernameNotFoundException;
+//import org.springframework.stereotype.Service;
+//
+//@Service
+//public class CustomUserDetailsService implements UserDetailsService {
+//
+//	@Autowired
+//	RessourceHumaineRepository RHRepository;
+//	@Autowired
+//	RessourceHumaineRoleRepository RHRoleRepository;
+//
+//	@Override
+//	public UserDetails loadUserByUsername(String arg0) throws UsernameNotFoundException {
+//		Optional<RessourceHumaine> opt= RHRepository.findByLogin(arg0);
+//		if (!opt.isPresent()) {
+//			throw new UsernameNotFoundException("utilisateur inconnu");
+//		}
+//		return new CustomUserDetails(opt.get(), RHRoleRepository.findCustomRoleByLogin(arg0));
+//
+//	}
+//
+//}

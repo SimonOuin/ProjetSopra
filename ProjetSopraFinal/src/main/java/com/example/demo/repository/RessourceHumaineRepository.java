f744f6ab41de79ba3c70cded9ea3a3dd4a5cccfd
Unknown <alexisrelin@gmail.com>
2018-06-11 10:27:06 +0200

    repository rh

---
+++ ProjetSopraFinal/src/main/java/com/example/demo/repository/RessourceHumaineRepository.java
@@ -0,0 +1,20 @@
+package com.example.demo.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+
+import model.Adresse;
+import model.RessourceHumaine;
+
+public interface RessourceHumaineRepository extends JpaRepository<RessourceHumaine, Long>{
+
+	
+	public RessourceHumaine findByNom(@Param ("nom") String nom);	
+	public RessourceHumaine findByPrenom(@Param ("prenom") String prenom);
+	public RessourceHumaine findByAdresse(@Param ("adresse") Adresse adresse);
+	public RessourceHumaine findByTel(@Param ("tel") Integer tel);
+	public RessourceHumaine findByEmail(@Param ("email") String email);
+	public RessourceHumaine findByLogin(@Param ("login") String login);
+	public RessourceHumaine findByMdp(@Param ("mdp") String mdp);
+	
+}
